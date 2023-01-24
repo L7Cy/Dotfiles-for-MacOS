59ec58508d9b554dc27c1593299122bb66b4e787
L7Cy <l7cy.at@gmail.com>
2023-01-24 10:32:26 +0900

    remove guifont setting

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -38,7 +38,6 @@ local options = {
 	background = "dark",
 	scrolloff = 9999,
 	sidescrolloff = 8,
-	guifont = "monospace:h17",
 	splitbelow = false, -- オンのとき、ウィンドウを横分割すると新しいウィンドウはカレントウィンドウの下に開かれる
 	splitright = false, -- オンのとき、ウィンドウを縦分割すると新しいウィンドウはカレントウィンドウの右に開かれる
 }
